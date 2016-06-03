69fab2894b07ce5791cbff3d8852d313a081ce75
root <root@ebc72f987d4b>
2016-06-03 06:22:34 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,10 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "defaultLanguage" : "en",
+  "displayName" : "web_3rdJune",
+  "homePage" : "Main",
+  "name" : "web_3rdjune",
+  "platformType" : "WEB",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
